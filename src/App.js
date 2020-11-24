c71da471bcd5b01f1335dcf1e61ed84162feeb0e
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-11-24 10:05:40 +0200

    adds task 1 render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import "./App.css";
+import './App.css';
+import UserProfile from './components/social-profile/user-profile';
+import user from './components/social-profile/user.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <h2>task 1</h2>
+      <UserProfile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
